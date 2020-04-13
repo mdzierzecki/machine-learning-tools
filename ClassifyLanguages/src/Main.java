2511d319a9fe721a1099b9b5095d5941868bbc4c
Mateusz Dzierzecki <mateusz@dzierzecki.com>
2020-04-13 12:20:08 +0200

    add reader; files to vector

--- ClassifyLanguages/src/Main.java
+++ ClassifyLanguages/src/Main.java
@@ -1,19 +1,35 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
 
-        Vector vc = new Vector("cz", "languages/czech/1");
-        System.out.println(vc.getText());
-        System.out.println(vc.getNormalizedText());
+        Vector vc = new Vector("de", "languages/german/1");
 
-        // {65=7, 66=3, 67=10, 68=2, 73=1, 74=7, 75=2, 77=6, 78=7, 79=2, 80=5, 83=7, 84=1, 85=1, 86=33, 87=10, 90=4, 97=366, 98=61, 99=159, 100=138, 101=485, 102=10, 103=6, 104=85, 105=306, 106=89, 107=141, 108=137, 109=162, 110=266, 111=329, 112=135, 114=144, 115=246, 116=241, 117=160, 118=157, 119=3, 121=108, 122=157}
+        ArrayList<Vector> result = textsToVectors();
 
-        System.out.println(vc.getVector());
+        System.out.println(result);
 
 
     }
+
+
+    private static ArrayList<Vector> textsToVectors() {
+        ArrayList<Vector> result = new ArrayList<>();
+        for (int i=1; i<=10; i++){
+
+            Vector vc = new Vector("de", "languages/german/" + i);
+            result.add(vc);
+
+            Vector vc2 = new Vector("cz", "languages/czech/" + i);
+            result.add(vc2);
+
+            Vector vc3 = new Vector("en", "languages/english/" + i);
+            result.add(vc3);
+        }
+
+        return result;
+    }
+
 
 
 }
