3a0c50030ddde824031a3d230e793735acc2bc13
Mateusz Dzierzecki <mateusz@dzierzecki.com>
2020-04-29 19:38:40 +0200

    add repo for k-means algorithm

---
+++ K-means/src/Record.java
@@ -0,0 +1,16 @@
+public class Record {
+    private int Number;
+    private double sepalLength;
+    private double sepalWidth;
+    private double petalLength;
+    private double petalWidth;
+    private String result;
+
+    public Record(int number, double sepalLength, double sepalWidth, double petalLength, double petalWidth) {
+        Number = number;
+        this.sepalLength = sepalLength;
+        this.sepalWidth = sepalWidth;
+        this.petalLength = petalLength;
+        this.petalWidth = petalWidth;
+    }
+}
